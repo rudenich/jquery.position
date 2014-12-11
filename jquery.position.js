1fb2e610ec50911af4ec43a64970c553f135cd17
Rudenich <rudenich@gmail.com>
2014-12-11 15:17:29 +0200

    refactoring

--- jquery.position.js
+++ jquery.position.js
@@ -1,50 +1,48 @@
 (function($){
-    var utils = {
-        above:function(targetY,targetHeight,height){
-            return {y:targetY-height};
+    $.position = {
+        utils : {
+            before:function(targetPos,targetSize,size){
+                var result = targetPos-size;
+                return result;
+            },
+            begin:function(targetPos,targetSize,size){
+                var result = targetPos
+                return result;
+            },
+            center:function(targetPos,targetSize,size){
+                var result = targetPos+(targetSize-size)/2;
+                return result;
+            },
+            end:function(targetPos,targetSize,size){
+                var result = targetPos+targetSize-size;
+                return result;
+            },
+            after:function(targetPos,targetSize,size){
+                var result = targetPos+targetSize;
+                return result;
+            }
         },
-        top:function(targetY,targetHeight,height){
-            return {y:targetY};
+        mapX:{
+            before:'before',
+            left:'begin',
+            center:'center',
+            right:'end',
+            after:'after'
         },
-        middle:function(targetY,targetHeight,height){
-            var y = targetY+(targetHeight-height)/2;
-            return {y:y};
-        },
-        bottom:function(targetY,targetHeight,height){
-            var y = targetY+targetHeight-height;
-            return {y:y};
-        },
-        under:function(targetY,targetHeight,height){
-            var y = targetY+targetHeight;
-            return {y:y};
-        },
-
-
-        before:function(targetX,targetWidth,width){
-            var x = targetX-width;
-            return {x:x};
-        },
-        left:function(targetX,targetWidth,width){
-            var x = targetX
-            return {x:x};
-        },
-        center:function(targetX,targetWidth,width){
-            var x = targetX+(targetWidth-width)/2;
-            return {x:x};
-        },
-        right:function(targetX,targetWidth,width){
-            var x = targetX+targetWidth-width;
-            return {x:x};
-        },
-        after:function(targetX,targetWidth,width){
-            var x = targetX+targetWidth;
-            return {x:x};
+        mapY : {
+            above:'before',
+            top:'begin',
+            middle:'center',
+            bottom:'end',
+            under:'after'
         }
     };
+
 $.fn.position = function(conf){
     var target = conf.target;
-    var x = conf.left;
-    var y = conf.top;
+    var position = conf.position;
+    var x = position[0];
+    var y = position[1];
 
     var targetY = target.offset().top;
     var targetHeight = target.height();
@@ -54,11 +52,12 @@ $.fn.position = function(conf){
     var targetWidth = target.width();
     var width = this.width();
 
-    var top = utils[y](targetY,targetHeight,height);
-    var left = utils[x](targetX,targetWidth,width);
+
+    var top = $.position.utils[$.position.mapY[y]](targetY,targetHeight,height);
+    var left = $.position.utils[$.position.mapX[x]](targetX,targetWidth,width);
     this.css({
-        top:top.y,
-        left:left.x
+        top:top,
+        left:left
     });
 }
 
